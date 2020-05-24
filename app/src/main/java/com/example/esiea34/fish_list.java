ccc3b65d00f3578c3933ef467fc7287af48bdc24
T2Clubber <ghenassia@et.esiea.fr>
2020-05-24 13:34:20 +0200

    Stockage de données en cache

--- app/src/main/java/com/example/esiea34/fish_list.java
+++ app/src/main/java/com/example/esiea34/fish_list.java
@@ -4,12 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,14 +28,39 @@ public class fish_list extends AppCompatActivity {
     private RecyclerView recyclerView;
     private ListAdapteur mAdapter;
     private RecyclerView.LayoutManager layoutManager;
+    private SharedPreferences sharedPreferences;
+    private Gson gson;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fish_list);
 
+        sharedPreferences = getSharedPreferences("application_esiea", Context.MODE_PRIVATE);
+        gson = new GsonBuilder()
+                .setLenient()
+                .create();
 
-       makeApiCall();
+        List<Pokemon> pokemonList = getDataFromCache();
+
+        if(pokemonList != null) {
+            showList(pokemonList);
+        } else {
+            makeApiCall();
+        }
+
+    }
+
+    private List<Pokemon> getDataFromCache() {
+
+        String jsonPokemon = sharedPreferences.getString("jsonPokemonList", null);
+
+        if (jsonPokemon == null){
+            return null;
+        } else {
+            Type listType = new TypeToken<List<Pokemon>>(){}.getType();
+            return gson.fromJson(jsonPokemon, listType);
+        }
     }
 
     private void showList(List<Pokemon> pokemonList) {
@@ -58,9 +87,6 @@ public class fish_list extends AppCompatActivity {
     private static final String BASE_URL = "https://pokeapi.co/";
 
     private void makeApiCall() {
-            Gson gson = new GsonBuilder()
-                    .setLenient()
-                    .create();
 
             Retrofit retrofit = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
@@ -78,7 +104,9 @@ public class fish_list extends AppCompatActivity {
                     if(response.isSuccessful() && response.body() != null){
                         List<Pokemon> pokemonList = response.body().getResults();
                        // Toast.makeText(getApplicationContext(),"API Success", Toast.LENGTH_SHORT).show();
+                        saveList(pokemonList);
                         showList(pokemonList);
+
                     } else {
                         showError();
                     }
@@ -91,6 +119,20 @@ public class fish_list extends AppCompatActivity {
             });
 
         }
+
+    private void saveList(List<Pokemon> pokemonList) {
+
+        String jsonString = gson.toJson(pokemonList);
+
+        sharedPreferences
+                .edit()
+                .putString("jsonPokemonList", "jsonString")
+                .apply();
+
+        Toast.makeText(getApplicationContext(),"List saved", Toast.LENGTH_SHORT).show();
+
+
+    }
 
     private void showError() {
         Toast.makeText(getApplicationContext(),"API Error", Toast.LENGTH_SHORT).show();
